70cf4abdee88320b99d28d073edebb2b29aefd2d
shinhwa <kangell@alertra.com>
2021-02-26 20:55:56 -0600

    Added Amass install here just as a stop-gap until I figure out how to install it with the rest of the Go modules.

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,3 +3,6 @@
 # Put any misc configuration here.
 
 git config --global pull.rebase false
+
+# I can't get this to work in the gomodules section, so we'll just do it live.
+GO111MODULE=on go get -v github.com/OWASP/Amass/v3/...
